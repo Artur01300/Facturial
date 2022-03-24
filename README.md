3dc9ec651f8e32a01a3881f5567ff4c77c9e69f2
Artur01300 <artur.karapetyan@outlook.fr>
2022-03-24 13:44:46 +0100

    1er commit

---
+++ README.md
@@ -0,0 +1,5 @@
+*** Facturiel
+
+<p>
+    C'est une petite application, qui permet de générer des factures et des divises.
+</p>
---
+++ src/script.ts
@@ -0,0 +1,59 @@
+class FormInput {
+    form: HTMLFormElement;
+    type: HTMLSelectElement
+    firstName: HTMLInputElement;
+    lastName: HTMLInputElement;
+    address: HTMLInputElement;
+    country: HTMLInputElement;
+    town: HTMLInputElement;
+    zip: HTMLInputElement;
+    product: HTMLInputElement;
+    price: HTMLInputElement;
+    quantity: HTMLInputElement;
+    tva: HTMLInputElement;
+    constructor(){
+        this.form = document.getElementById('form')as HTMLFormElement;
+        this.type = document.getElementById('type')as HTMLSelectElement;
+        this.firstName = document.getElementById('firstName') as HTMLInputElement;
+        this.lastName =  document.getElementById('lastName') as HTMLInputElement;
+        this.address =  document.getElementById('address') as HTMLInputElement;
+        this.country =  document.getElementById('country') as HTMLInputElement;
+        this.town =  document.getElementById('town') as HTMLInputElement;
+        this.zip =  document.getElementById('zip') as HTMLInputElement;
+        this.product =  document.getElementById('product') as HTMLInputElement;
+        this.price =  document.getElementById('price') as HTMLInputElement;
+        this.quantity =  document.getElementById('quantity') as HTMLInputElement;
+        this.tva =  document.getElementById('tva') as HTMLInputElement;
+
+        this.listenEventEndSupbit();
+    }
+    private listenEventEndSupbit(){
+        this.form.addEventListener('submit', (e:Event)=>{
+            e.preventDefault();
+            const inputs = this.inputDatas();
+
+            if(Array.isArray(inputs)){
+                //Pour extrère les valeur depuis le variable inputs
+                const [type, firstName, lastName, address, country, town, 
+                    zipe, product, price, quantity, tva
+                ] = inputs;
+                // les variables qui sont éxtraient
+                console.log('%cscript.ts line:42 inputs', 'color: #007acc;', type, firstName, lastName, address, country, town, zipe, product, price, quantity, tva);
+            }
+        });
+    }
+    private inputDatas(): [string, string, string, string, string, string, string, string, number, number, number] | void{
+
+        if(this.price.valueAsNumber > 0 && this.quantity.valueAsNumber > 0 && this.tva.valueAsNumber > 0 ){
+            return [
+                this.type.value, this.firstName.value, this.lastName.value, this.address.value, this.country.value, this.town.value, 
+                this.zip.value, this.product.value, this.price.valueAsNumber, this.quantity.valueAsNumber, this.tva.valueAsNumber
+            ];
+        }
+        else{
+            alert('Les valeurs numériques doivent être > 0 !');
+            return;
+        }
+    }
+}
+new FormInput();
